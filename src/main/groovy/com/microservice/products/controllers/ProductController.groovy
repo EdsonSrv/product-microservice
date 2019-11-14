9519b200ed7f0ae287801580fdefaff832929749
Edson Servin <edson@ciencias.unam.mx>
2019-11-14 12:50:33 -0600

    coding master Adding the detail endpoint

--- src/main/groovy/com/microservice/products/controllers/ProductController.groovy
+++ src/main/groovy/com/microservice/products/controllers/ProductController.groovy
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.beans.factory.annotation.Autowired
 import com.microservice.products.service.ProductService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import com.microservice.products.domain.Product
 
 @RestController
@@ -15,6 +16,11 @@ class ProductController {
   @GetMapping("/list")
   List<Product> list(){
     productService.findAll()
+  }
+
+  @GetMapping("/detail/{id}")
+  Product detail(@PathVariable Long id){
+    productService.findById(id)
   }
 
 }
--- src/main/groovy/com/microservice/products/service/ProductService.groovy
+++ src/main/groovy/com/microservice/products/service/ProductService.groovy
@@ -2,6 +2,7 @@ package com.microservice.products.service
 
 import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.transaction.annotation.Transactional
 import com.microservice.products.repo.ProductRepository
 import com.microservice.products.domain.Product
 
@@ -11,12 +12,14 @@ class ProductService {
   @Autowired
   ProductRepository productRepository
 
+  @Transactional(readOnly = true)
   List<Product> findAll(){
     productRepository.findAll()
   }
 
+  @Transactional(readOnly = true)
   Product findById(Long id){
-    productRepository.findById(id)
+    productRepository.findById(id).orElse(null)
   }
 
 }
